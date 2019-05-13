e452776f9ba963b9e1d081aca2601125c0d09f92
gaohuiqin <gaoyuanjava@163.com>
2019-05-13 17:08:19 +0800

    新增部门管理controller

--- src/main/java/com/lanxin/controller/EmpController.java
+++ src/main/java/com/lanxin/controller/EmpController.java
@@ -4,6 +4,9 @@ package com.lanxin.controller;
  * Created by Administrator on 2019/5/13 0013.
  */
 public class EmpController {
+
+   private String testing;
+
     private String username;
 
     private String password;
